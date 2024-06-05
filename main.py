2cdb216a23dc49f7c015a693f995f45741b99c2b
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-06-05 10:33:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Read the JSON file once at startup
 with open('fcc_data.json', 'r') as json_file:
@@ -19,3 +35,4 @@ async def get_record(record_id: int):
     if record:
         return record
     return JSONResponse(status_code=404, content={"message": "Record not found"})
+

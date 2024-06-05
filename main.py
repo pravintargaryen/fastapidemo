e61d10b7ce75c42c5f8d90439d1ff9d46aa7a95d
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-06-05 10:41:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ import json
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
+    '*'
 ]
 
 app.add_middleware(
